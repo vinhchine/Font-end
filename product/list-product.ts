0088f31940b933322dfca415963662208031a313
vinhchine <tranvuvinh99@gmail.com>
2019-03-29 21:37:21 +0700

    Update list-product.ts

--- product/list-product.ts
+++ product/list-product.ts
@@ -1,7 +1,7 @@
 
 import { Product } from './product';
 export const PRODUCTS: Product[] = [
-  { productID: 1, productName: 'Mr. Nice' },
+  { productID: 5, productName: 'Mr. Nice' },
   { productID: 2, productName: 'Narco' },
   { productID: 3, productName: 'Bombasto' },
   { productID: 4, productName: 'Celeritas' },
